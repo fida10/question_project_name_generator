8546c28ef1806193b5780aca089caff66899f08c
eshad <fidaeshad10@gmail.com>
2023-12-08 12:01:07 -0600

    first commit, working exec

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Write};
+
+fn main() {
+    let mut chapter_name = String::new();
+    let mut lesson_name = String::new();
+
+    print!("Enter chapter name: ");
+    io::stdout().flush().unwrap(); // Making sure the print! macro outputs immediately
+    io::stdin().read_line(&mut chapter_name).expect("Failed to read line");
+    chapter_name = chapter_name.trim().replace(|c: char| !c.is_alphanumeric(), "");
+
+    print!("Enter lesson name: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut lesson_name).expect("Failed to read line");
+    lesson_name = lesson_name.trim().replace(|c: char| !c.is_alphanumeric(), "");
+
+    let combined = format!("{}_{}", chapter_name, lesson_name);
+    println!("Combined string: {}", combined);
+}
