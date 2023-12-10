17803bd01c85523f0f98c276d1f0ce7f07e38873
eshad <fidaeshad10@gmail.com>
2023-12-10 11:18:45 -0600

    added looping mechanism, exit and reuse previous chapter names

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
-use std::{io::{self, Write}, thread, time::Duration};
+use std::io::{self, Write};
 
 fn main() {
     let mut chapter_name = String::new();
-    let mut lesson_name = String::new();
+    let mut rerun = true;
 
-    print!("Enter chapter name: ");
-    io::stdout().flush().unwrap(); // Making sure the print! macro outputs immediately
-    io::stdin().read_line(&mut chapter_name).expect("Failed to read line");
-    chapter_name = chapter_name.trim().replace(|c: char| !c.is_alphanumeric(), "");
+    while rerun {
+        let mut lesson_name = String::new();
 
-    print!("Enter lesson name: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut lesson_name).expect("Failed to read line");
-    lesson_name = lesson_name.trim().replace(|c: char| !c.is_alphanumeric(), "");
+        print!("Enter chapter name OR enter \'exit\' to exit; leave blank and press enter to use previous chapter name: ");
+        io::stdout().flush().unwrap(); // Making sure the print! macro outputs immediately
+        let mut temp_chap_name: String = String::new();
+        io::stdin()
+            .read_line(&mut temp_chap_name)
+            .expect("Failed to read line");
+        if temp_chap_name.eq("\n") {
+            println!("Using previous chapter name: {}", chapter_name)
+        } else if temp_chap_name.eq("exit") {
+            println!("Exiting!");
+            rerun = false;
+            continue;
+        } else {
+            chapter_name = temp_chap_name;
+        }
 
-    let combined = format!("{}_{}", chapter_name, lesson_name).to_lowercase();
-    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    println!("Combined string: {}", combined);
-    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    println!("Waiting 10 seconds before closing...");
-    thread::sleep(Duration::from_secs(10));
+        println!("chapter name: {}", chapter_name);
+
+        chapter_name = chapter_name
+            .trim()
+            .replace(|c: char| !c.is_alphanumeric(), "");
+
+        print!("Enter question name: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut lesson_name)
+            .expect("Failed to read line");
+        lesson_name = lesson_name
+            .trim()
+            .replace(|c: char| !c.is_alphanumeric(), "");
+
+        let combined = format!("{}_{}", chapter_name, lesson_name).to_lowercase();
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        println!("Combined string: {}", combined);
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
 }
